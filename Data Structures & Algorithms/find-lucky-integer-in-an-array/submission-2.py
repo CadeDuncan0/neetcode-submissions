2ec6a2f390b646ad91f1d0b54adbf77aff45a7e1
Cade Duncan <cadeduncan72@gmail.com>
2026-06-27 18:07:36 -0700

    Add: find-lucky-integer-in-an-array - submission-2

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        hmap = {}
+        for num in arr:
+            hmap[num] = hmap.get(num, 0) + 1
+
+        ans = -1
+        for key, value in hmap.items():
+            if key == value and key > ans:
+                ans = key
+
+        return ans
